06cc8163b474d757c4a5abc7bd17968ac8e7e989
Chengjie Shi <shicjie@bu.edu>
2026-09-16 14:08:18 -0400

    EDG signed by michael

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-/// Your crew's name. Both of you are going to change this line.
-const CREW_NAME: &str = "Michael";
+e/// Your crew's name. Both of you are going to change this line.
+const CREW_NAME: &str = "EDG";
 
 /// Your crew's motto. You will both change this one too, earlier and separately.
 const MOTTO: &str = "a great motto";
@@ -10,7 +10,7 @@ fn main() {
     println!("Crew roster:");
 
     // ROSTER: replace the line below with one for yourself.
-    println!("  (Mackey has signed on)");
+    println!("  (Mackey and michael has signed on)");
 
     println!();
     println!("Motto: {}", MOTTO);
